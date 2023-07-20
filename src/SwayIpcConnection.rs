2f04ffaf964fb21d05d7696039f0c450156e18cc
Shekhov Ismoil <veyxov@gmail.com>
2023-07-20 09:32:50 +0500

    refactor: cleanup and extract sway ipc connection

---
+++ src/SwayIpcConnection.rs
@@ -0,0 +1,56 @@
+use super::find_socket;
+use swayipc::Connection;
+
+
+use std::env;
+
+pub(crate) struct Sway {
+    pub(crate) connection: Option<Connection>,
+}
+
+impl Sway {
+    pub fn new() -> Sway {
+        Sway { connection: None }
+    }
+
+    pub(crate) fn connect(&mut self) {
+        if let None = self.connection {
+            if let Err(env::VarError::NotPresent) = env::var("SWAYSOCK") {
+                let path = match find_socket() {
+                    Some(path) => path,
+                    None => {
+                        println!("Failed to locate a SWAYSOCK from /run/user/1000/sway-ipc.*");
+                        return;
+                    }
+                };
+                println!("$SWAYSOCK is not set. Defaulting to \"{}\"", path);
+                env::set_var("SWAYSOCK", path);
+            }
+
+            match Connection::new() {
+                Ok(connection) => self.connection = Some(connection),
+                Err(e) => println!("SwayClient#connect() failed: {}", e),
+            }
+        }
+    }
+
+    pub(crate) fn current_application(&mut self) -> Option<String> {
+        self.connect();
+        let connection = match &mut self.connection {
+            Some(connection) => connection,
+            None => return None,
+        };
+
+        if let Ok(node) = connection.get_tree() {
+            if let Some(node) = node.find_focused(|n| n.focused) {
+                if node.app_id.is_some() {
+                    return node.app_id;
+                } else if let Some(wp) = node.window_properties {
+                    return wp.class;
+                }
+            }
+        }
+        None
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -3,65 +3,14 @@ use serde::{Deserialize, Serialize};
 use simplelog::*;
 use std::fs::read_dir;
 use std::os::unix::prelude::OsStrExt;
-use std::{env, str};
+use std::{str};
 
 use std::io::Write;
 use std::net::{SocketAddr, TcpStream};
-use std::process::{Command, Stdio};
 use std::str::FromStr;
 use std::time::Duration;
 
-use swayipc::Connection;
-
-struct Sway {
-    connection: Option<Connection>,
-}
-
-impl Sway {
-    pub fn new() -> Sway {
-        Sway { connection: None }
-    }
-
-    fn connect(&mut self) {
-        if let None = self.connection {
-            if let Err(env::VarError::NotPresent) = env::var("SWAYSOCK") {
-                let path = match find_socket() {
-                    Some(path) => path,
-                    None => {
-                        println!("Failed to locate a SWAYSOCK from /run/user/1000/sway-ipc.*");
-                        return;
-                    }
-                };
-                println!("$SWAYSOCK is not set. Defaulting to \"{}\"", path);
-                env::set_var("SWAYSOCK", path);
-            }
-
-            match Connection::new() {
-                Ok(connection) => self.connection = Some(connection),
-                Err(e) => println!("SwayClient#connect() failed: {}", e),
-            }
-        }
-    }
-
-    fn current_application(&mut self) -> Option<String> {
-        self.connect();
-        let connection = match &mut self.connection {
-            Some(connection) => connection,
-            None => return None,
-        };
-
-        if let Ok(node) = connection.get_tree() {
-            if let Some(node) = node.find_focused(|n| n.focused) {
-                if node.app_id.is_some() {
-                    return node.app_id;
-                } else if let Some(wp) = node.window_properties {
-                    return wp.class;
-                }
-            }
-        }
-        None
-    }
-}
+mod SwayIpcConnection;
 
 fn find_socket() -> Option<String> {
     let uid = 1000;
@@ -114,7 +63,7 @@ fn main() {
     log::info!("successfully connected");
     let writer_stream = kanata_conn.try_clone().expect("clone writer");
 
-    let mut sway = Sway::new();
+    let mut sway = SwayIpcConnection::Sway::new();
     sway.connect();
     main_loop(writer_stream, sway);
 }
@@ -160,7 +109,7 @@ impl FromStr for ServerMessage {
     }
 }
 
-fn main_loop(mut s: TcpStream, mut sway: Sway) {
+fn main_loop(mut s: TcpStream, mut sway: SwayIpcConnection::Sway) {
     loop {
         let cur_win_name = sway.current_application().unwrap();
 
@@ -170,24 +119,6 @@ fn main_loop(mut s: TcpStream, mut sway: Sway) {
     }
 }
 
-fn _read_from_kanata_old(mut s: TcpStream) {
-    loop {
-        let result = get_sway_wininfo();
-        let deserialized = serde_json::from_str::<Vec<WinInfo>>(&result).unwrap();
-
-        // TODO: Only get focused window to remove the for loop
-        for win in deserialized.iter() {
-            if win.is_focused {
-                // PERF: Optimize
-                let layer = win.name.clone().trim().to_lowercase().to_owned();
-
-                write_to_kanata(layer, &mut s);
-            }
-        }
-
-        std::thread::sleep(Duration::from_millis(500));
-    }
-}
 
 fn write_to_kanata(new: String, s: &mut TcpStream) {
     //log::error!("focused window: {}", win.name);
@@ -199,28 +130,4 @@ fn write_to_kanata(new: String, s: &mut TcpStream) {
     if wsz != expected_wsz {
         panic!("failed to write entire message {wsz} {expected_wsz}");
     }
-}
-
-fn get_sway_wininfo() -> String {
-    let output = Command::new("swaymsg")
-        .arg("--raw")
-        .arg("-t")
-        .arg("get_tree")
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to execute command");
-
-    let jq_command = "[.nodes[].nodes[].nodes[] | {name: .name, is_focused: .focused}]";
-
-    let jq_output = Command::new("jq")
-        .arg(jq_command)
-        .arg("--raw-output")
-        .stdin(Stdio::from(output.stdout.unwrap())) // Pipe through.
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Failed to execute jq command");
-
-    let output = jq_output.wait_with_output().unwrap();
-    let result = str::from_utf8(&output.stdout).unwrap();
-    result.to_owned()
 }
