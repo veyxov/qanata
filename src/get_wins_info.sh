494a267f8e473c71c272a5e9c960c410a6c1f0b7
Shekhov Ismoil <veyxov@gmail.com>
2023-07-21 09:48:04 +0500

    doc: added notes to the swaymsg shell command

--- src/get_wins_info.sh
+++ src/get_wins_info.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Requires: swaymsg, jq
+# Note: now using swayipc so this is not needed; usefull for debugging purposes
 swaymsg --raw -t get_tree | jq '.nodes[].nodes[].nodes[] | {name: .name, is_focused: .focused}'
