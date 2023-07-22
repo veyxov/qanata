a805c822f5b9e523825afdd9505c99016d34c8a6
Shekhov Ismoil <veyxov@gmail.com>
2023-07-22 18:36:04 +0500

    feat: bar color based on the frequency

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -5,6 +5,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.cm as cm
 
 # Step 1: Save the provided data into a text file named "key_events.txt".
 
@@ -22,15 +23,20 @@ with open(filename, "r") as file:
             sent_key = sent_key.replace("KEY_", "")  # Remove the "KEY_" prefix
             sent_keys_count[sent_key] = sent_keys_count.get(sent_key, 0) + 1
 
-# Step 3: Generate the heatmap.
+# Step 3: Generate the heatmap with color-coded bars.
 keys = list(sent_keys_count.keys())
 counts = list(sent_keys_count.values())
 
+# Get the color map and normalize the counts for color coding
+cmap = cm.get_cmap('plasma')
+normalize = plt.Normalize(vmin=min(counts), vmax=max(counts))
+colors = [cmap(normalize(value)) for value in counts]
+
 fig, ax = plt.subplots(figsize=(10, 6))
-heatmap = ax.bar(keys, counts, color='b')
+heatmap = ax.bar(keys, counts, color=colors)  # Use colors for bars
 ax.set_xlabel("Sent Keys")
 ax.set_ylabel("Count")
-ax.set_title("Sent Keys Heatmap")
+ax.set_title("Sent Keys Heatmap (Color-coded)")
 
 # Annotate each bar with its count value.
 for bar in heatmap:
