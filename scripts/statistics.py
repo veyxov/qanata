1e42ab8ef68dda5e2fe83f45900f9012a3ec83f3
Shekhov Ismoil <veyxov@gmail.com>
2023-07-22 18:30:42 +0500

    refactor: better visualization

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -3,42 +3,43 @@
 # NOTE: please forgive me, I'm not a python dev 😁
 # oooooh, I'm a python dev now 😎
 
-import re
-import pandas as pd
-
-# Function to extract key code and event type from the log data
-def parse_log(log_data):
-    keys = []
-    events = []
-    pattern = r"code:\s(KEY_\w+),\svalue:\s(\w+)"
-    matches = re.findall(pattern, log_data)
-    for match in matches:
-        keys.append(match[0])
-        events.append(match[1])
-    return keys, events
-
-# Step 1: Read the log data from the file
-file_path = "/home/iz/.local/bin/kanata/log"
-with open(file_path, 'r') as file:
-    log_data = file.read()
-
-# Step 2: Parse the log data into DataFrame
-keys, events = parse_log(log_data)
-df = pd.DataFrame({"Key": keys, "Event": events})
-
 import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
 
-# Create a pivot table to count key press frequency
-heatmap_data = df.pivot_table(index='Event', columns='Key', aggfunc='size', fill_value=0)
+# Step 1: Save the provided data into a text file named "key_events.txt".
 
-# Sort the columns by frequency to arrange the heatmap
-heatmap_data = heatmap_data[heatmap_data.sum().sort_values(ascending=False).index]
+# Step 2: Read data from the file and process it.
+filename = "/home/iz/.local/bin/kanata/log"
 
-# Create the heatmap
-plt.figure(figsize=(12, 8))
-sns.heatmap(heatmap_data, cmap='YlGnBu', annot=True, fmt='d', cbar=False)
-plt.title('Keyboard Heatmap - Key Press Frequency')
-plt.xlabel('Key')
-plt.ylabel('Event')
+# Dictionary to store the count of each sent key
+sent_keys_count = {}
+
+with open(filename, "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        if "SENT=" in line:
+            sent_key = line.split("SENT=")[1].strip()
+            sent_keys_count[sent_key] = sent_keys_count.get(sent_key, 0) + 1
+
+# Step 3: Generate the heatmap.
+keys = list(sent_keys_count.keys())
+counts = list(sent_keys_count.values())
+
+fig, ax = plt.subplots(figsize=(10, 6))
+heatmap = ax.bar(keys, counts, color='b')
+ax.set_xlabel("Sent Keys")
+ax.set_ylabel("Count")
+ax.set_title("Sent Keys Heatmap")
+
+# Annotate each bar with its count value.
+for bar in heatmap:
+    height = bar.get_height()
+    ax.annotate('{}'.format(height),
+                xy=(bar.get_x() + bar.get_width() / 2, height),
+                xytext=(0, 3),
+                textcoords="offset points",
+                ha='center', va='bottom')
+
+plt.tight_layout()
 plt.show()
+
