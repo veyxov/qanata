8b3567aaf3966a4e88f66ad7502e9e64b26c2d78
Shekhov Ismoil <veyxov@gmail.com>
2023-07-23 19:52:57 +0500

    refactor: added arguments to statistics script

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -6,49 +6,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.cm as cm
+import argparse
 
 # Step 1: Save the provided data into a text file named "key_events.txt".
 
 # Step 2: Read data from the file and process it.
-filename = "/home/iz/.local/bin/kanata/log"
+filename = "/home/iz/kanata.log"
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", help="Specify the file path")
+args = parser.parse_args()
+if args.file:
+    file_name = args.file
+    # Now you can use the 'file_name' variable in your code.
+    # Dictionary to store the count of each sent key
+    sent_keys_count = {}
 
-# Dictionary to store the count of each sent key
-sent_keys_count = {}
+    with open(filename, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            if "SENT=" in line:
+                sent_key = line.split("SENT=")[1].strip()
+                sent_key = sent_key.replace("KEY_", "")  # Remove the "KEY_" prefix
+                sent_keys_count[sent_key] = sent_keys_count.get(sent_key, 0) + 1
 
-with open(filename, "r") as file:
-    lines = file.readlines()
-    for line in lines:
-        if "SENT=" in line:
-            sent_key = line.split("SENT=")[1].strip()
-            sent_key = sent_key.replace("KEY_", "")  # Remove the "KEY_" prefix
-            sent_keys_count[sent_key] = sent_keys_count.get(sent_key, 0) + 1
+    # Step 3: Generate the heatmap with color-coded bars.
+    keys = list(sent_keys_count.keys())
+    counts = list(sent_keys_count.values())
 
-# Step 3: Generate the heatmap with color-coded bars.
-keys = list(sent_keys_count.keys())
-counts = list(sent_keys_count.values())
+    # Get the color map and normalize the counts for color coding
+    cmap = cm.get_cmap('plasma')
+    normalize = plt.Normalize(vmin=min(counts), vmax=max(counts))
+    colors = [cmap(normalize(value)) for value in counts]
 
-# Get the color map and normalize the counts for color coding
-cmap = cm.get_cmap('plasma')
-normalize = plt.Normalize(vmin=min(counts), vmax=max(counts))
-colors = [cmap(normalize(value)) for value in counts]
+    fig, ax = plt.subplots(figsize=(12, 6))  # Adjust the figure size
+    heatmap = ax.bar(keys, counts, color=colors)  # Use colors for bars
+    ax.set_xlabel("Sent Keys")
+    ax.set_ylabel("Count")
+    ax.set_title("Sent Keys Heatmap (Color-coded)")
 
-fig, ax = plt.subplots(figsize=(12, 6))  # Adjust the figure size
-heatmap = ax.bar(keys, counts, color=colors)  # Use colors for bars
-ax.set_xlabel("Sent Keys")
-ax.set_ylabel("Count")
-ax.set_title("Sent Keys Heatmap (Color-coded)")
+    # Rotate the x-axis labels for better readability
+    plt.xticks(rotation=45, ha='right')
 
-# Rotate the x-axis labels for better readability
-plt.xticks(rotation=45, ha='right')
+    # Annotate each bar with its count value.
+    for bar in heatmap:
+        height = bar.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(bar.get_x() + bar.get_width() / 2, height),
+                    xytext=(0, 3),
+                    textcoords="offset points",
+                    ha='center', va='bottom')
 
-# Annotate each bar with its count value.
-for bar in heatmap:
-    height = bar.get_height()
-    ax.annotate('{}'.format(height),
-                xy=(bar.get_x() + bar.get_width() / 2, height),
-                xytext=(0, 3),
-                textcoords="offset points",
-                ha='center', va='bottom')
-
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
+else:
+    print("No file provided\nPlease specify --file")
