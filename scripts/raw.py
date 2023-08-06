b777cce7cb2b3b51fa5763d19711c859514aefe3
Shekhov Ismoil <veyxov@gmail.com>
2023-08-06 10:26:13 +0500

    feat(statistics): it shows some kind of heatmap

--- scripts/raw.py
+++ scripts/raw.py
@@ -1,7 +1,43 @@
 import shutil
 import matplotlib.pyplot as plt
 import seaborn as sns
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import matplotlib.widgets as widgets
+import pandas as pd
 
+def plot_keys_counts(key_count, key_type):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(x=list(key_count.keys()), y=list(key_count.values()), ax=ax)
+    ax.set_xlabel(f"{key_type} Key")
+    ax.set_ylabel("Count")
+    ax.set_title(f"{key_type} Key Counts")
+    plt.xticks(rotation=45)
+
+    def on_actual_keys_button_click(event):
+        update_plot(key_count, "Actual")
+
+    def on_sent_keys_button_click(event):
+        update_plot(resulting_key_count, "Sent")
+
+    ax_actual_keys = plt.axes([0.7, 0.03, 0.1, 0.05])
+    ax_sent_keys = plt.axes([0.81, 0.03, 0.1, 0.05])
+
+    actual_keys_button = widgets.Button(ax_actual_keys, "Actual Keys")
+    actual_keys_button.on_clicked(on_actual_keys_button_click)
+
+    sent_keys_button = widgets.Button(ax_sent_keys, "Sent Keys")
+    sent_keys_button.on_clicked(on_sent_keys_button_click)
+
+    plt.show()
+
+def update_plot(key_count, key_type):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(x=list(key_count.keys()), y=list(key_count.values()), ax=ax)
+    ax.set_xlabel(f"{key_type} Key")
+    ax.set_ylabel("Count")
+    ax.set_title(f"{key_type} Key Counts")
+    plt.xticks(rotation=45)
+    plt.show()
 
 def parse_data(line):
     parts = line.strip().split("|")
@@ -19,6 +55,19 @@ def parse_data(line):
         }
     else:
         return None
+
+def create_heatmap(resulting_key_count):
+    # Convert resulting_key_count to a DataFrame
+    df = pd.DataFrame(list(resulting_key_count.items()), columns=["Key", "Count"])
+
+    # Create a pivot table to reshape the DataFrame
+    df_pivot = df.pivot_table(index="Key", columns="Key", values="Count", fill_value=0)
+
+    # Create the heatmap
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(df_pivot, annot=True, fmt="d", cmap="Blues")
+    plt.title("Keyboard Key Usage Heatmap (Sent Keys)")
+    plt.show()
 
 def calculate_statistics(data):
     # Count the occurrences of each actual key
@@ -68,6 +117,7 @@ def plot_layer_counts(layer_count):
     plt.xticks(rotation=45)
     plt.show()
 
+
 def main():
     file_path = "/home/iz/kanata.log"  # Replace with the path to your file
     backup_file_path = "/home/iz/kanata.log.backup"
@@ -101,10 +151,9 @@ def main():
     for layer, count in layer_count.items():
         print(f"{layer}: {count}")
 
-    # Plot visualizations
-    plot_key_counts(key_count)
-    plot_resulting_key_counts(resulting_key_count)
-    plot_layer_counts(layer_count)
+    # Plot interactive visualization with buttons
+    create_heatmap(resulting_key_count)
+
 
 if __name__ == "__main__":
     main()
